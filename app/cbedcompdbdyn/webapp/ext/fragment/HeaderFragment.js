0b887875471b8ceb8f6e9b3eb373448c8f994b2f
Jhonson Rozario <jhonsonr-v@tataprojects.com>
2024-02-13 06:03:18 +0000

    excel button

--- app/cbedcompdbdyn/webapp/ext/fragment/HeaderFragment.js
+++ app/cbedcompdbdyn/webapp/ext/fragment/HeaderFragment.js
@@ -3,9 +3,9 @@ sap.ui.define([
     "sap/ui/export/Spreadsheet",
     "sap/ui/model/json/JSONModel",
     'sap/ui/export/library',
-], function (MessageToast, Spreadsheet,JSONModel,exportLibrary) {
+], function (MessageToast, Spreadsheet, JSONModel, exportLibrary) {
     'use strict';
-    	var EdmType = exportLibrary.EdmType;
+    var EdmType = exportLibrary.EdmType;
     return {
         onRowExpand: function (oEvent) {
             debugger
@@ -90,26 +90,79 @@ sap.ui.define([
         onExportExcel: function (oEvent) {
             debugger;
             // Parse the data from Excel into an array
-           var data = [];
-        
-            var oModel = new JSONModel(data);
-        
+            var data = [];
+
+            // var oModel = new JSONModel(data);
+            //mainhbiox
+            var mainhbox = sap.ui.getCore().byId("cbedcompdbdyn::Project_DetailsObjectPage--fe::CustomSubSection::Fragment--mainhbox1");
+
+            //project desc
+            let project_desc = sap.ui.getCore().byId("cbedcompdbdyn::Project_DetailsObjectPage--fe::CustomSubSection::Fragment--projectvalue").getText();
+
+
+            let table = sap.ui.getCore().byId("cbedcompdbdyn::Project_DetailsObjectPage--fe::CustomSubSection::Fragment--itemstable1");
+            let table_items = [];
+
+            for (let i = 0; i < table.getItems().length; i++) {
+                table_items.push({
+                    mta_no:(i+1),
+                    item_desc: table.getItems()[i].getCells()[0].getText(),
+                    cap_each: table.getItems()[i].getCells()[1].getText(),
+                    dia: table.getItems()[i].getCells()[2].getText(),
+                    tl_tl: table.getItems()[i].getCells()[3].getText(),
+                    moc: table.getItems()[i].getCells()[4].getText(),
+                    dpb: table.getItems()[i].getCells()[5].getText(),
+                    weights: table.getItems()[i].getCells()[6].getText(),
+                    quantity: table.getItems()[i].getCells()[7].getText(),
+                })
+
+            }
+            var data = [
+                { a:"Commercial Bid Evaluation"},
+                { a:"item",b:'Air Storage Tanks - 6 Nos.'},
+                { a:"indent",b:'Awaited'},
+                { a:"project",b:project_desc,i:" "},
+                { a:"Client",b:'ISRO -TWT'},
+                { a:"",b:''},
+                { a:"",b:'',f:"Cylindrical Data"},
+                { a:"MTO Sr.NO ",b:'Items Description',e:"Capacity Each ( Cu Mtr )",f:"Dia mm",g:"TL to TL Length (mm)",h:"MOC",i:"Design Pressure bar",j:"Weights",k:"Quantity"},
+               ];
+
+               for (let i = 0; i < table_items.length; i++) {
+                    data.push({
+                        a:table_items[i].mta_no,
+                        b:table_items[i].item_desc,
+                        // c:table_items[i].,
+                        // d:table_items[i].mta_no,
+                        e:table_items[i].cap_each,
+                        f:table_items[i].dia,
+                        g:table_items[i].tl_tl,
+                        h:table_items[i].moc,
+                        i:table_items[i].dpb,
+                        j:table_items[i].weights,
+                        k:table_items[i].quantity,
+                    })
+                
+               }
+            //     var udata = [];
+            //    udata.push(data);
+             
+            debugger
             // Update the columns array to match the columns in your Excel spreadsheet
             var oSpreadsheet = new sap.ui.export.Spreadsheet({
                 workbook: {
                     columns: [
-                        { label: 'Client', property: 'Client', type: EdmType.String },
-                        { label: 'Project', property: 'Project', type: EdmType.String },
-                        { label: 'Indent', property: 'Indent', type: EdmType.String },
-                        { label: 'Item No', property: 'itemno', type: EdmType.String },
-                        { label: 'Item Description', property: 'item_desc', type: EdmType.String },
-                        { label: 'Capacity Each (Cu Mtr)', property: 'capacity_each_in_cu_mtr', type: EdmType.String },
-                        { label: 'Dia (mm)', property: 'dia_in_mm', type: EdmType.String },
-                        { label: 'TL to TL Length (mm)', property: 'tl_to_tl_len', type: EdmType.String },
-                        { label: 'MOC', property: 'moc', type: EdmType.String },
-                        { label: 'Design Pressure (bar)', property: 'design_pressure_bar', type: EdmType.String },
-                        { label: 'Weights (Kgs)', property: 'weights_in_kg', type: EdmType.String },
-                        { label: 'Qty No', property: 'qty_no', type: EdmType.String }
+                        { label: ' ', property: 'a', type: EdmType.String },
+                        { label: 'B', property: 'b', type: EdmType.String },
+                        { label: 'C', property: 'c', type: EdmType.String },
+                        { label: 'D', property: 'd', type: EdmType.String },
+                        { label: 'E', property: 'e', type: EdmType.String },
+                        { label: 'F', property: 'f', type: EdmType.String },
+                        { label: 'G', property: 'g', type: EdmType.String },
+                        { label: 'H', property: 'h', type: EdmType.String },
+                        { label: 'I', property: 'i', type: EdmType.String },
+                        { label: 'J', property: 'j', type: EdmType.String },
+                        { label: 'K', property: 'k', type: EdmType.String },
                     ],
                     context: {
                         application: 'SAP UI5 Export Sample',
@@ -118,10 +171,10 @@ sap.ui.define([
                     hierarchyLevel: 'level'
                 },
                 // Pass the parsed data array to the dataSource property
-                dataSource: data,
+                dataSource: [data],
                 fileName: 'cbe_comp.xlsx' // Adjust the filename if needed
             });
-        
+
             oSpreadsheet.build()
                 .then(function () {
                     MessageToast.show("Exported successfully!");
@@ -130,12 +183,12 @@ sap.ui.define([
                     MessageToast.show("Export failed: " + reason);
                 });
         }
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
 
     };
 });
